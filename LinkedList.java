c0514c9ffe75bf86458de90c6799a029f6501fef
Giovanni Rivera <rivera.giovanni271@gmail.com>
2022-09-30 15:44:32 -0700

    [LinkedList Impl]: Add LinkedList.java stubs

---
+++ LinkedList.java
@@ -0,0 +1,39 @@
+package com.grivera64.datastructure.linkedlist;
+
+/*
+ * Name: Giovanni Rivera
+ * Instructor: Dr. Chatterjee
+ * Course: CSC 311-04
+ * Creation Date: 9/30/2022
+ * Due Date: 10/6/2022
+ * File Name: ArrayList.java
+ * Description: Implements the LinkedList ADT methods from our "bare-bones" List interface
+ */
+
+public class LinkedList<E> implements List<E> {
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
